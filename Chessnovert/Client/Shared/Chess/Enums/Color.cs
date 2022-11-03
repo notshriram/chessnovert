f767186ca2e42e91fbcbcf4057a42fd519a398a3
Shriram Ravindranathan <49370927+notshriram@users.noreply.github.com>
2022-11-03 16:42:09 +0530

    Add Chess Board Component and Styling

---
+++ Chessnovert/Client/Shared/Chess/Enums/Color.cs
@@ -0,0 +1,8 @@
+﻿namespace Chessnovert.Client.Shared.Chess.Enums
+{
+    public enum Color
+    {
+        White = 1,
+        Black = 2,
+    }
+}
---
+++ Chessnovert/Client/Shared/Chess/Piece.cs
@@ -0,0 +1,30 @@
+﻿using Chessnovert.Client.Shared.Chess.Enums;
+
+namespace Chessnovert.Client.Shared.Chess
+{
+    abstract class Piece
+    {
+        public Piece(Color color)
+        {
+            Color = color;
+        }
+        public abstract char Value { get; }
+        public Color Color { get; }
+
+        public abstract int Row { get; set; }
+        public abstract int Col { get; set; }
+
+        // Dest Row + Dest Col => New Coordinate Struct?
+        public bool IsLegal(int destinationRow, int destinationCol, Piece[][] Board)
+        {
+            Piece piece = Board[destinationRow][destinationCol];
+            if ((piece==null) || (this.Color!=piece.Color))
+            {
+                return CheckLegal(destinationRow, destinationCol, Board);
+            }
+            return false;
+        }
+        public abstract bool CheckLegal(int destinationRow, int destinationCol, Piece[][] Board);
+        
+    }
+}
